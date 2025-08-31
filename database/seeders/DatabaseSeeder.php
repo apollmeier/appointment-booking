d7d2e86f8ba3a6b3d00b5980687d53cb8a503e4a
Andre Pollmeier <pollmeier.a@gmail.com>
2025-08-31 20:55:46 +0200

    Seed predefined Specialization data in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Specialization;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,5 +12,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $specializationSequence = [
+            ['name' => 'Kardiologie'],
+            ['name' => 'Neurologie'],
+            ['name' => 'Orthopädie'],
+            ['name' => 'Psychiatrie'],
+            ['name' => 'Dermatologie'],
+        ];
+
+        $specializations = Specialization::factory()
+            ->count(count($specializationSequence))
+            ->sequence(...$specializationSequence)
+            ->create();
     }
 }
