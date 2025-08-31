590f490c6197399fd36ada378805b48f0d15ae21
Andre Pollmeier <pollmeier.a@gmail.com>
2025-08-31 22:12:32 +0200

    Add TimeSlotController with basic methods and define API routes for timeSlots index, show, and update

---
+++ app/Http/Controllers/Api/V1/TimeSlotController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\TimeSlotResource;
+use App\Models\TimeSlot;
+use Illuminate\Http\Request;
+
+class TimeSlotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+}
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\V1\DoctorController;
+use App\Http\Controllers\Api\V1\TimeSlotController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('doctors', DoctorController::class)->only(['index', 'show']);
+Route::apiResource('timeSlots', TimeSlotController::class)->except(['store', 'destroy']);
