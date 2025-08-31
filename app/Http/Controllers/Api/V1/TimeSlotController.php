dd21c9c29161a562394f379278a9f7dcb655dd73
Andre Pollmeier <pollmeier.a@gmail.com>
2025-08-31 22:25:07 +0200

    Use `findOrFail` in TimeSlotController@show and return transformed TimeSlotResource for API response.

--- app/Http/Controllers/Api/V1/TimeSlotController.php
+++ app/Http/Controllers/Api/V1/TimeSlotController.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\TimeSlotResource;
 use App\Models\TimeSlot;
+use App\Traits\ApiResponses;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class TimeSlotController extends Controller
 {
+    use ApiResponses;
+
     /**
      * Display a listing of the resource.
      */
@@ -22,7 +26,14 @@ class TimeSlotController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            return TimeSlotResource::make(TimeSlot::available()->findOrFail($id));
+        } catch (ModelNotFoundException $e) {
+            return $this->error([
+                'title' => 'Time slot not found',
+                'detail' => "Could not find available time slot with ID $id",
+            ], 404);
+        }
     }
 
     /**
