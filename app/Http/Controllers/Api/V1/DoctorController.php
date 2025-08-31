5928481c2a476995b1b9ade55549651ec74666d2
Andre Pollmeier <pollmeier.a@gmail.com>
2025-08-31 21:56:51 +0200

    Use `ApiResponses` trait in DoctorController and implement proper error handling in `show` method

--- app/Http/Controllers/Api/V1/DoctorController.php
+++ app/Http/Controllers/Api/V1/DoctorController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\DoctorResource;
 use App\Models\Doctor;
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Traits\ApiResponses;
 
 class DoctorController extends Controller
 {
+    use ApiResponses;
+
     /**
      * Display a listing of the resource.
      */
@@ -22,6 +25,13 @@ class DoctorController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            return DoctorResource::make(Doctor::findOrFail($id));
+        } catch (ModelNotFoundException $e) {
+            return $this->error([
+                'title' => 'Doctor not found',
+                'detail' => "Could not find doctor with ID $id",
+            ], 404);
+        }
     }
 }
