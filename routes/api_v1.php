6b37d886743d541d9f54dc44110d095120815cfd
Andre Pollmeier <pollmeier.a@gmail.com>
2025-08-31 22:27:59 +0200

    Add AppointmentController with basic CRUD methods and define API routes for appointments

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AppointmentController;
 use App\Http\Controllers\Api\V1\DoctorController;
 use App\Http\Controllers\Api\V1\TimeSlotController;
 use Illuminate\Support\Facades\Route;
@@ -7,3 +8,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('doctors', DoctorController::class)->only(['index', 'show']);
 Route::apiResource('timeSlots', TimeSlotController::class)->except(['store', 'destroy']);
+Route::apiResource('appointments', AppointmentController::class);
