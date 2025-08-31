941e1b444237f2d571aa24547bcb3e4c84217de8
Andre Pollmeier <pollmeier.a@gmail.com>
2025-08-31 21:55:54 +0200

    Remove unused `store` method from DoctorController for cleanup

--- app/Http/Controllers/Api/V1/DoctorController.php
+++ app/Http/Controllers/Api/V1/DoctorController.php
@@ -18,14 +18,6 @@ class DoctorController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(string $id)
