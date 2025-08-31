7a4ab36561f15145e71db736767214fd364c4558
Andre Pollmeier <pollmeier.a@gmail.com>
2025-08-31 22:12:54 +0200

    Add TimeSlotResource for transforming time slot data in the API

---
+++ app/Http/Resources/V1/TimeSlotResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TimeSlotResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'timeSlots',
+            'id' => $this->id,
+            'attributes' => [
+                'startTime' => $this->start_time,
+                'endTime' => $this->end_time,
+                'isAvailable' => $this->is_available,
+            ],
+            'relationships' => [
+                'doctor' => [
+                    'type' => 'doctors',
+                    'id' => $this->doctor_id,
+                ]
+            ],
+            'includes' => DoctorResource::make($this->doctor)
+        ];
+    }
+}
