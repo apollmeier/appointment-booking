7a0b71f149b6974d3a4b4a1bf84f3dc44a84e093
Andre Pollmeier <pollmeier.a@gmail.com>
2025-09-01 16:07:09 +0200

    Add AppointmentResource and StoreAppointmentRequest for transforming and validating appointment data in the API

---
+++ app/Http/Resources/V1/AppointmentResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AppointmentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'appointments',
+            'id' => $this->id,
+            'attributes' => [
+                'patientName' => $this->patient_name,
+                'patientEmail' => $this->patient_email,
+                'dateTime' => $this->date_time,
+                'status' => $this->status
+            ],
+            'relationships' => [
+                'type' => 'doctors',
+                'id' => $this->doctor_id
+            ],
+            'includes' => DoctorResource::make($this->doctor)
+        ];
+    }
+}
