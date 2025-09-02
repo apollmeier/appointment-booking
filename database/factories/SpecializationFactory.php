73da15286774ccd288ae6b29e9cdf4d06becb26a
Andre Pollmeier <pollmeier.a@gmail.com>
2025-09-02 22:35:29 +0200

    Set default `name` attribute in `SpecializationFactory` using Faker

--- database/factories/SpecializationFactory.php
+++ database/factories/SpecializationFactory.php
@@ -17,7 +17,7 @@ class SpecializationFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->jobTitle(),
         ];
     }
 }
